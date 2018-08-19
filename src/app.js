9c9e8a86176a4fb863602c9728ed32bb32f7be09
William Raimundo Azevedo <willianray@gmail.com>
2018-08-19 06:31:07 -0300

    Adicionar imagens.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 
@@ -9,6 +10,7 @@ const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', experiencesRouter);
 
--- src/models/mockDbExample.js
+++ src/models/mockDbExample.js
@@ -50,7 +50,7 @@ const candies = [
         id: 1,
         title: 'Pudim de melado',
         sinopse: '',
-        image: '',
+        image: 'http://192.168.25.43:3000/imgs/b77Cba0Be8524F78Dd58Dbc5B4A1Bd38@3x.png',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         city: {
             id: 1,
@@ -58,11 +58,11 @@ const candies = [
             state: 'Minas Gerais'
         },
         price: "R$ 99,00 por pessoa",
-        thumb: '',
+        thumb: 'http://192.168.25.43:3000/imgs/b77Cba0Be8524F78Dd58Dbc5B4A1Bd38.png',
         recorrence: 10,
         category: {
             name: 'doces',
-            image: ''
+            image: 'http://192.168.25.43:3000/imgs/doces@2x.png'
         },
         availability
     },
@@ -70,7 +70,7 @@ const candies = [
         id: 2,
         title: 'Pé de moleque da Neuzinha',
         sinopse: '',
-        image: '',
+        image: 'http://192.168.25.43:3000/imgs/b7712@3x.png',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         city: {
             id: 1,
@@ -78,11 +78,11 @@ const candies = [
             state: 'Pernambuco'
         },
         price: "R$ 23,00 por pessoa",
-        thumb: '',
+        thumb: 'http://192.168.25.43:3000/imgs/b7712.png',
         recorrence: 1,
         category: {
             name: 'doces',
-            image: ''
+            image: 'http://192.168.25.43:3000/imgs/doces@2x.png'
         },
         availability
     },
@@ -90,7 +90,7 @@ const candies = [
         id: 3,
         title: 'Bala delícia da Nani',
         sinopse: '',
-        image: '',
+        image: 'http://192.168.25.43:3000/imgs/b123@3x.png',
         description: 'Oportunidade incrível de conhecer a fábrica de doces de leites junto de uma pessoa local.',
         city: {
             id: 1,
@@ -98,11 +98,11 @@ const candies = [
             state: 'Minas Gerais'
         },
         price: "R$ 15,00 por pessoa",
-        thumb: '',
+        thumb: 'http://192.168.25.43:3000/imgs/b123.png',
         recorrence: 5,
         category: {
             name: 'doces',
-            image: ''
+            image: 'http://192.168.25.43:3000/imgs/doces@2x.png'
         },
         availability
     }
@@ -113,7 +113,7 @@ const drinks = [
         id: 4,
         title: 'Alambique',
         sinopse: '',
-        image: '',
+        image: 'http://192.168.25.43:3000/imgs/bebidas@3x.png',
         description: 'Oportunidade incrível de conhecer o alambique reponsável pela fabricação da cachaçá pitú.',
         city: {
             id: 1,
@@ -121,15 +121,14 @@ const drinks = [
             state: 'Minas Gerais'
         },
         price: "R$ 150 por pessoa",
-        thumb: '',
+        thumb: 'http://192.168.25.43:3000/imgs/bebidas.png',
         recorrence: 1,
         category: {
-            name: 'drinks',
+            name: 'bebidas',
             image: ''
         }
     }
 ];
-
 //mock experiences.
 const experiences = [
     ...candies,
